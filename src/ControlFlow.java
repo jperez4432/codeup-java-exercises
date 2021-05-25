9ceb2176200fdf4dfec033e035a3132e6139e654
Juan Perez <jperez4432@gmail.com>
2021-05-25 14:30:11 -0500

    completed ex 1

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -8,13 +8,32 @@ public class ControlFlow {
 //        }
 //
 
-            int i = 1;
-            while (i < 10) {
-                System.out.println("Now im in the loop, i as not incremented yet and its value is " + i);
-                i++;
-                System.out.println("Im still in the loop, but i has incremented now and its value is " +i );
-            }
-        System.out.println("outside the loop after its executed. the conditional has tested false. the value of i is now " + i);
+//            int i = 1;
+//            while (i < 10) {
+//                System.out.println("Now im in the loop, i as not incremented yet and its value is " + i);
+//                i++;
+//                System.out.println("Im still in the loop, but i has incremented now and its value is " +i );
+//            }
+//        System.out.println("outside the loop after its executed. the conditional has tested false. the value of i is now " + i);
+
+//1a
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i++);
+//        }
+//1b
+//        for(int i = 2; i < 100; i += 2) {
+//            System.out.println(i);
+//        }
+
+// 1b-3
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        }while(i < 1000000);
+
+
 
     }
 }

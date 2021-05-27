3ac528136a46b8135f3902afcbc6e09e6be73605
Juan Perez <jperez4432@gmail.com>
2021-05-27 11:10:57 -0500

    completed exercise 1

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -10,15 +10,18 @@ public class MethodsExercises {
         return num1 - num2;
     }
 
-
-
     public static int multiply(int num1, int num2) {
         return num1 * num2;
     }
-
+    public static int divide(int num1, int num2) {
+        return num1/num2;
+    }
 
     public static void main(String[] args) {
         System.out.println(multiply(1, 2));
+        System.out.println(add(20,10));
+        System.out.println(subtract(30, 19));
+        System.out.println(divide(100, 10));
     }
 }
 

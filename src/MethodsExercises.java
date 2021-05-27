2b6263a18902888c6b310252b68647ededb42a96
Juan Perez <jperez4432@gmail.com>
2021-05-27 11:07:35 -0500

    completed 1.a/b

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,6 +2,10 @@ import org.w3c.dom.ls.LSOutput;
 
 public class MethodsExercises {
 
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
     public static int multiply(int num1, int num2) {
         return num1 * num2;
     }

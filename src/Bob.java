bdbe75fded37e4fa616c2ca2934146c719e83e22
Juan Perez <jperez4432@gmail.com>
2021-05-26 21:31:17 -0500

    need to see why empty string is being returned after do while

--- src/Bob.java
+++ src/Bob.java
@@ -4,14 +4,22 @@ public class Bob {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ask Bob anything");
-        String userInput = sc.nextLine();
-        if (userInput.endsWith("?")) {
-            System.out.println("Sure");
-        } else if (userInput.endsWith("!")) {
-            System.out.println("Whoa, Chill out!");
-        } else {
-            System.out.println("Fine. Be that way!");
-        }
+        String response;
+        do {
+            System.out.print("Ask Bob anything\n");
+            String userInput = sc.nextLine();
+
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, Chill out!");
+            } else if (userInput == "") {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+            System.out.println("Would you like to keep trying to talk to Bob? Answer yes/no");
+            response = sc.next();
+        } while (response.equals("yes") || response.equals("Yes"));
     }
 }

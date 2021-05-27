68ef601d738fc3568725b145cf559937a7b80a9d
Juan Perez <jperez4432@gmail.com>
2021-05-27 11:06:08 -0500

    completed setup

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,5 +1,14 @@
-public class MethodsExercises {
-    public static void main(String[] args) {
+import org.w3c.dom.ls.LSOutput;
 
+public class MethodsExercises {
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(multiply(1, 2));
     }
 }
+

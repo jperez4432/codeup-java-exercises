fd6ede69b749970cd31149474dcbe121afc09663
Juan Perez <jperez4432@gmail.com>
2021-05-27 11:08:52 -0500

    completed 1-abc

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -6,6 +6,12 @@ public class MethodsExercises {
         return num1 + num2;
     }
 
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+
+
     public static int multiply(int num1, int num2) {
         return num1 * num2;
     }

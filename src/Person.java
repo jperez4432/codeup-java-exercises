529556d3cc59243687413751f94b029d3dff1863
Juan Perez <jperez4432@gmail.com>
2021-06-01 18:36:55 -0500

    pre study hall

--- src/Person.java
+++ src/Person.java
@@ -3,6 +3,7 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Scanner;
 
 public class Person {
+
     private String name;
 
      public static String getName(String name) {
@@ -10,7 +11,7 @@ public class Person {
     }
 
     public static void setName(String name) {
-
+         name = name;
     }
 
     public void sayHello() {
@@ -20,11 +21,16 @@ public class Person {
     public Person(String name) {
          this.name = name;
     }
+
     public Person(){}
 
     public static void main(String[] args) {
          Person testPerson = new Person("Juan");
+         Person testPerson2 = new Person("Juan");
+         testPerson.name = setName();
         System.out.println(getName(testPerson.name));
+
+      //  System.out.println(setName("Jason"));
         // System.out.println(testPerson.getName());
     }
 }

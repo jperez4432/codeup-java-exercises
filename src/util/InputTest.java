24be24641ae301e3fd7c5c4c1d69bbba548bdbe9
Juan Perez <jperez4432@gmail.com>
2021-06-02 10:16:26 -0500

    added input test

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,5 +1,12 @@
 package util;
 
 public class InputTest {
+    public static void main(String[] args) {
+        Input il = new Input();
 
+        il.getString();
+        il.yesOrNo();
+        il.getInt(1,100);
+        il.getDouble();
+    }
 }

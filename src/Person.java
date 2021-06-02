b52ec0a4b9b3c29d9d49adb57914c0e7781586fa
Juan Perez <jperez4432@gmail.com>
2021-06-01 21:51:08 -0500

    completed 1.2

--- src/Person.java
+++ src/Person.java
@@ -24,6 +24,10 @@ public class Person {
 
     public Person(){}
 
+
+
+
+
     public static void main(String[] args) {
          Person testPerson = new Person("Juan");
         // Person testPerson2 = new Person("Juan");
@@ -33,6 +37,22 @@ public class Person {
         testPerson.setName("ramon");
         System.out.println(testPerson.getName());
 
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
 
       //  System.out.println(setName("Jason"));
         // System.out.println(testPerson.getName());

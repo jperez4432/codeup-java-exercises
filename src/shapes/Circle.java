714a1fc30741f32c718420f7e1c1d40897e8b63c
Juan Perez <jperez4432@gmail.com>
2021-06-01 22:39:31 -0500

    ending for night

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,5 +1,17 @@
 package shapes;
 
 public class Circle {
+    private int Radius;
 
 }
+    public Circle(double radius) {
+
+    }
+        // write method to get the area
+    public double getArea() {
+
+    }
+        // write method to find the circumference
+    public double getCircumference() {
+
+    }

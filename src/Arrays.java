912128aab803c3f5d65ec982ae468791384821bc
Juan Perez <jperez4432@gmail.com>
2021-06-03 09:16:49 -0500

    began arrays

---
+++ src/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = {14, 64, 34, 44};
+            for (int n : numbers) {
+                System.out.println(n);
+            }
+        }
+    }
+

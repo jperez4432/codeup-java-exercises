97f810b4f6348e974e3f889f4034c742004d33ab
Juan Perez <jperez4432@gmail.com>
2021-06-03 09:51:53 -0500

    fixed array issue

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,4 +1,5 @@
-public class Arrays {
+import java.util.Arrays;
+public class Array {
     public static void main(String[] args) {
         int[] numbers = {14, 64, 34, 44};
         numbers[1] = 33;
@@ -8,7 +9,7 @@ public class Arrays {
             System.out.println(n);
         }
 
-        String[] topFiveBoyNames = new String[5];
+        String[] topFiveBoyNames = new String[6];
         topFiveBoyNames[0] = "Greg";
         topFiveBoyNames[1] = "Jim";
         topFiveBoyNames[2] = "Geoff";
@@ -17,5 +18,31 @@ public class Arrays {
         for (String n : topFiveBoyNames) {
             System.out.println(n);
         }
+        boolean[] lookingAtDefaultVals = new boolean[3];
+
+        for (boolean boo : lookingAtDefaultVals) {
+            System.out.println(boo);
+        }
+
+        Arrays.fill(lookingAtDefaultVals, true);
+
+        for (boolean boo : lookingAtDefaultVals) {
+            System.out.println(boo);
+        }
+
+        int[] alsoNums = {14,33,34,44};
+        for (int n : numbers) {
+            System.out.println(n);
+        }
+
+        for (int n : alsoNums) {
+            System.out.println(n);
+        }
+
+        System.out.println(Arrays.equals(numbers, alsoNums));
+
+        int[] yetMoreNums = Arrays.copyOf(numbers, alsoNums);
+
+
     }
 }

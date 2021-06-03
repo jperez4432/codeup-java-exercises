82111115c9fefe2f0dd09d8a0fb5906b534889ef
Juan Perez <jperez4432@gmail.com>
2021-06-03 13:18:29 -0500

    minor edit

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -7,10 +7,6 @@ public class ArraysExercises {
     }
 
     public static void main(String[] args) {
-        // int[] numbers = {1, 2, 3, 4, 5};
-        // System.out.println(Arrays.toString(numbers));
-
-        //String[]  Person = {"Barry", "Deion", "Randy"};
         Person[] people = new Person[3];
         people[0] = new Person("Barry");
         people[1] = new Person("Deion");

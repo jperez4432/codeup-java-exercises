e88a01c4d010d7340dee636eab88555804b3ab93
Juan Perez <jperez4432@gmail.com>
2021-06-03 13:52:39 -0500

    javi lec

---
+++ src/Bird.java
@@ -0,0 +1,33 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Bird {
+    // can it fly?
+    private boolean canFly;
+
+    public boolean isCanFly() {
+        return canFly;
+    }
+
+    public void setCanFly(boolean canFly) {
+
+        this.canFly = canFly;
+    }
+    //what kind of bird?
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void makeNoise() {
+        System.out.println("Tweet tweet");
+    }
+
+    public void move() {
+        System.out.println("flap flap");
+    }
+}
---
+++ src/BirdTest.java
@@ -0,0 +1,5 @@
+public class BirdTest {
+    public static void main(String[] args) {
+
+    }
+}

0d0e558ac5423d8d41d6be9dc9df80b5355121c9
Juan Perez <jperez4432@gmail.com>
2021-06-05 18:09:22 -0500

    practice java

---
+++ src/practice.java
@@ -0,0 +1,19 @@
+public class practice {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        int almostPi = (int) pi;
+        int favoriteNumber = 44;
+        long myNumber;
+       // System.out.println(myNumber);
+        String meow = "Meow meow meowdy meow meow";
+        System.out.println(meow);
+        System.out.println(favoriteNumber);
+        System.out.println("Hello World!");
+        System.out.println(pi);
+        System.out.println(almostPi);
+        {
+            System.out.println("Code inside of curly braces should be indented");
+            System.out.println("With 4 spaces, aka a tab dont be a space guy");
+        }
+    }
+}

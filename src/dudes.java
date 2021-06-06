62e1f728e51ae86b3377fa010f13ec7a080c0746
Juan Perez <jperez4432@gmail.com>
2021-06-05 23:30:25 -0500

    trying to comprehend the this keyword

--- src/dudes.java
+++ src/dudes.java
@@ -1,22 +1,36 @@
+class dudes {
+    private String name;
 
-    class dudes {
-        public static long worldPopulation = 7500000000L; // class property
-        public String name; // instance property
-
-        public static void main(String[] args) {
-            dudes theBestDrummerAlive = new dudes();
-            theBestDrummerAlive.name = "Neil Peart";
-
-            dudes.worldPopulation += 1; // accessing a static property
-
-            System.out.println(dudes.worldPopulation); // 7500000001
-
-            // technically this will work, but it is usually better to reference
-            // static properties through the class, not instances
-            System.out.println(theBestDrummerAlive.name); // 7500000001
-
-            // DONT DO THIS, it will not compile
-           // System.out.println(Person.name);
-        }
+    public dudes(String name) {
+        this.name = name;
     }
+
+    public String sayHello() {
+        return  this.name + "%s says hello!" ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+//    class dudes {
+//        public static long worldPopulation = 7500000000L; // class property
+//        public String name; // instance property
+//
+//        public static void main(String[] args) {
+//            dudes theBestDrummerAlive = new dudes();
+//            theBestDrummerAlive.name = "Neil Peart";
+//
+//            dudes.worldPopulation += 1; // accessing a static property
+//
+//            System.out.println(dudes.worldPopulation); // 7500000001
+//
+//            // technically this will work, but it is usually better to reference
+//            // static properties through the class, not instances
+//            System.out.println(theBestDrummerAlive.name); // 7500000001
+//
+//            // DONT DO THIS, it will not compile
+//           // System.out.println(Person.name);
+//        }
+//    }
 
--- src/practice.java
+++ src/practice.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 
 public class practice {
-    public static long getPower(int base, int exponent) {
-        if (exponent == 0) {
-            return 1;
-        } else if (exponent == 1) {
-            return base;
-        } else if (exponent == 2) {
-            return base * base;
-        }
-        return base * getPower(base, exponent - 1);
-    }
 
-    public static void main(String[] args) {
-        System.out.println(getPower(3, 3));
-    }
+
+
+
+
+//    public static long getPower(int base, int exponent) {
+//        if (exponent == 0) {
+//            return 1;
+//        } else if (exponent == 1) {
+//            return base;
+//        } else if (exponent == 2) {
+//            return base * base;
+//        }
+//        return base * getPower(base, exponent - 1);
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(getPower(3, 3));
+//    }
 //    public static void count(int n) {
 //        if (n <= 0) {
 //            System.out.println("All done!");

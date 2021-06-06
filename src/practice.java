a37642c14b107c884fc26a949d8c6cf5c40749c8
Juan Perez <jperez4432@gmail.com>
2021-06-05 21:20:39 -0500

    redoing java 1 material

--- src/practice.java
+++ src/practice.java
@@ -3,8 +3,14 @@ public class practice {
         double pi = 3.14159;
         int almostPi = (int) pi;
         int favoriteNumber = 44;
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        //int three = (int) o;
         long myNumber;
        // System.out.println(myNumber);
+        System.out.println(theNumberThree);
+        System.out.println(o);
+      //  System.out.println(three);
         String meow = "Meow meow meowdy meow meow";
         System.out.println(meow);
         System.out.println(favoriteNumber);

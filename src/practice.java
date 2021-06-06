851f7ee343b319dd6ad4473e19f4ee7f7d1df6d8
Juan Perez <jperez4432@gmail.com>
2021-06-05 21:26:33 -0500

    completed sc

--- src/practice.java
+++ src/practice.java
@@ -1,25 +1,47 @@
+import java.util.Scanner;
+
 public class practice {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        int almostPi = (int) pi;
-        int favoriteNumber = 44;
-        String theNumberThree = "three";
-        Object o = theNumberThree;
-        //int three = (int) o;
-        long myNumber;
-       // System.out.println(myNumber);
-        System.out.println(theNumberThree);
-        System.out.println(o);
-      //  System.out.println(three);
-        String meow = "Meow meow meowdy meow meow";
-        System.out.println(meow);
-        System.out.println(favoriteNumber);
-        System.out.println("Hello World!");
-        System.out.println(pi);
-        System.out.println(almostPi);
-        {
-            System.out.println("Code inside of curly braces should be indented");
-            System.out.println("With 4 spaces, aka a tab dont be a space guy");
-        }
+
+
+
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number");
+//        int userInput = sc.nextInt();
+//        System.out.println("You entered " + userInput);
+//
+//        System.out.println("enter three words");
+//
+//        String one = sc.next();
+//        String two = sc.next();
+//        String three = sc.next();
+//
+//        System.out.printf("You entered %s %s %s", one, two, three);
+
+
+//        double pi = 3.14159;
+//        int almostPi = (int) pi;
+//        int favoriteNumber = 44;
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        //int three = (int) o;
+//        long myNumber;
+//       // System.out.println(myNumber);
+//        System.out.println(theNumberThree);
+//        System.out.println(o);
+//      //  System.out.println(three);
+//        String meow = "Meow meow meowdy meow meow";
+//        System.out.println(meow);
+//        System.out.println(favoriteNumber);
+//        System.out.println("Hello World!");
+//        System.out.println(pi);
+//        System.out.println(almostPi);
+//        {
+//            System.out.println("Code inside of curly braces should be indented");
+//            System.out.println("With 4 spaces, aka a tab dont be a space guy");
+//        }
     }
 }

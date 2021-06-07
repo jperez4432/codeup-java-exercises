6ebbb81d133e3847670f9a43efd69e3dda97889a
Juan Perez <jperez4432@gmail.com>
2021-06-07 09:40:28 -0500

    working on #2

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,15 @@
+package grades;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GradesApplication {
+    public static void main(String[] args) {
+        HashMap<String, String> students = new HashMap<>();
+
+        students.put("jperez4432", "90, 95, 100");
+        students.put("Coder4Life", );
+        students.put("HelloWorld", );
+        students.put("FastFingers", );
+        System.out.println(students);
+    }
+}

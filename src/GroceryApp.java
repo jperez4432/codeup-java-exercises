b5fa96c5ccb5b84a9a9838e72e4be122aaa0cb99
Juan Perez <jperez4432@gmail.com>
2021-06-07 21:45:01 -0500

    setup for grocery app

---
+++ src/GroceryApp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class GroceryApp {
+    public static void main(String[] args) {
+        System.out.println("Would you like to create a grocery list? Enter Yes/No");
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.next();
+        if (answer.equalsIgnoreCase("yes")) {
+
+        }
+    }
+}

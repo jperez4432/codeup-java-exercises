f6281b57c67460d0a28f9071d3e76fab5c0a264c
Juan Perez <jperez4432@gmail.com>
2021-06-07 21:20:34 -0500

    got list to display properly, just need to adjust the if statement

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -7,39 +7,39 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class GradesApplication extends Student {
-    private static Object Student;
+    // private static Object Student;
 
     public GradesApplication(String name) {
         super(name);
     }
 
-    public static void commandLine( ArrayList<String > keySet, HashMap <String, Student> students) {
+    public static String commandLine( ArrayList<String > list, HashMap <String, Student> students) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome!\nHere are the Github usernames of our students:");
-        System.out.println(keySet);
+        System.out.println(list);
         System.out.println("What student would you like to see more information on?");
         String entry = sc.next();
-        if (keySet.contains(entry)) {
+        if (list.contains(entry)) {
             System.out.println("We do have him available!");
-            System.out.println(students.get(entry).getName());
-            System.out.println(entry);
-            System.out.println(students.get(entry).getGradeAverage());
-        } else if (!keySet.contains(entry)) {
+            System.out.println("Name: " + students.get(entry).getName());
+            System.out.println("Username: " + entry);
+            System.out.println("Grade average: " + students.get(entry).getGradeAverage());
+        } else if (!list.contains(entry)) {
             System.out.printf("I'm sorry, but we do not have a user by the name of %s ", entry);
             System.out.println("would you like to try again? Enter Yes/No");
             String answer = sc.next();
             if (answer.equalsIgnoreCase("yes")) {
                 System.out.println("OK!");
-                return commandLine();
             }
         }
+        return entry;
     }
 
     public static void main(String[] args) {
 
         HashMap<String, Student> students = new HashMap<>();
         Set<String> keySet = students.keySet();
-        ArrayList<String> listOfKeys = new ArrayList<String>(keySet);
+
 
         Student student1 = new Student("Juan");
         students.put("jperez4432", student1);
@@ -48,7 +48,6 @@ public class GradesApplication extends Student {
         student1.addGrade(93);
         //   System.out.println(student1.getName());
         // student1.getGradeAverage();
-
         // System.out.println(students);
         Student student2 = new Student("Jeff");
         students.put("CoderGuy", student2);
@@ -68,8 +67,11 @@ public class GradesApplication extends Student {
         student4.addGrade(87);
         student4.addGrade(82);
       //student4.getGradeAverage();
-        commandLine((ArrayList<String>) keySet, students);
+        ArrayList<String> usernameList = new ArrayList<String>(keySet);
+        commandLine(usernameList, students);
 
+    }
+}
 
 
 //           System.out.println(students);
@@ -101,5 +103,3 @@ public class GradesApplication extends Student {
 //            }
 //        } while ()
 //        }
-    }
-}
